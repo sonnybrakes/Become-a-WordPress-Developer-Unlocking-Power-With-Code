981ae240981d168e8889090adec9fd0daeb9a4f3
Michel Frechette <mifrec@hotmail.com>
2019-03-13 09:11:01 -0400

    5-19a navigation menus COMMENTED OUT

--- functions.php
+++ functions.php
@@ -12,9 +12,9 @@
   add_action('wp_enqueue_scripts', 'university_files');
 
   function university_features() {
-    register_nav_menu('headerMenuLocation', 'Header Menu Location');
-    register_nav_menu('footerLocationOne', 'footer Location One');
-    register_nav_menu('footerLocationTwo', 'footer Location Two');
+    // register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    // register_nav_menu('footerLocationOne', 'footer Location One');
+    // register_nav_menu('footerLocationTwo', 'footer Location Two');
 
     add_theme_support('title-tag');
   }
--- header.php
+++ header.php
@@ -16,18 +16,18 @@
           <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
           <div class="site-header__menu group">
             <nav class="main-navigation">
-              <?php
+              <!-- <?php
                 wp_nav_menu(array(
                   'theme_location' => 'headerMenuLocation'
                 ));
-              ?>
-              <!-- <ul>
+              ?> -->
+              <ul>
                 <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
                 <li><a href="#">Programs</a></li>
                 <li><a href="#">Events</a></li>
                 <li><a href="#">Campuses</a></li>
                 <li><a href="#">Blog</a></li>
-              </ul> -->
+              </ul>
             </nav>
             <div class="site-header__util">
               <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
