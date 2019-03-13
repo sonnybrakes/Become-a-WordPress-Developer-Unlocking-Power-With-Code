2112165a3c26781ebc10fd53a0569173686574e6
Michel Frechette <mifrec@hotmail.com>
2019-03-13 09:21:00 -0400

    5-19 navigation menus

--- header.php
+++ header.php
@@ -16,8 +16,8 @@
           <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
           <div class="site-header__menu group">
             <nav class="main-navigation">
-              <ul>
-                <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+                <ul>
+                <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 16) echo 'class= "current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
                 <li><a href="#">Programs</a></li>
                 <li><a href="#">Events</a></li>
                 <li><a href="#">Campuses</a></li>
