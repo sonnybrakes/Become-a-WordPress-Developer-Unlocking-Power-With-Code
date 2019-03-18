c69b245c91ffc750ac48b30f39f003bbd8be7934
Michel Frechette <mifrec@hotmail.com>
2019-03-18 09:35:26 -0400

    6-22-B blog archives

--- archive.php
+++ archive.php
@@ -7,14 +7,16 @@ get_header(); ?>
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title"><?php if (is_category()) {
+    <h1 class="page-banner__title"><?php the_archive_title(); ?>
+      <!-- <?php if (is_category()) {
       single_cat_title();
-    }
+      }
       if (is_author()) {
-      echo 'Posts by: '; the_author();
-      } ?></h1>
+      echo 'Post by: '; the_author();
+      } ?> -->
+    </h1>
     <div class="page-banner__intro">
-      <p>Keep up with our latest news.</p>
+      <p><?php the_archive_description(); ?></p>
     </div>
   </div>
 </div>
